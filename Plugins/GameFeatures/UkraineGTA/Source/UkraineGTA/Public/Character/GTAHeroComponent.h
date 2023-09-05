c0814c8f5f9fc7dc26fdfa5f3b560b87dd625747
Anton Zhukov <janton20001@gmail.com>
2023-09-05 16:24:27 +0300

    Added default movement to character

---
+++ Plugins/GameFeatures/UkraineGTA/Source/UkraineGTA/Public/Character/GTAHeroComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/LyraHeroComponent.h"
+#include "GTAHeroComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UKRAINEGTA_API UGTAHeroComponent : public ULyraHeroComponent
+{
+	GENERATED_BODY()
+	
+};
