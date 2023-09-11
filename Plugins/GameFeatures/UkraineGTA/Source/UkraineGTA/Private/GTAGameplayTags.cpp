5fd3994e46e2922bd8496b4fc1c01c3d6eaec33c
Anton Zhukov <janton20001@gmail.com>
2023-09-11 13:58:41 +0300

    Created GTA Native tags + decreased hunger debuff

---
+++ Plugins/GameFeatures/UkraineGTA/Source/UkraineGTA/Private/GTAGameplayTags.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GTAGameplayTags.h"
+
+namespace GTAGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(UkraineGTA_Ability_Debuff_LowHunger, "UkraineGTA.Ability.Debuff.LowHunger", "Low hunger debuff decreasing HP of character")
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(UkraineGTA_Ability_Debuff_LowStamina, "UkraineGTA.Ability.Debuff.LowStamina", "Low stamina removes ability to jump/run")
+}
---
+++ Plugins/GameFeatures/UkraineGTA/Source/UkraineGTA/Public/GTAGameplayTags.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GTAGameplayTags
+{
+	UKRAINEGTA_API FGameplayTag FindTagByString(const FString& TagString, bool bMatchPartialString = false);
+
+	UKRAINEGTA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(UkraineGTA_Ability_Debuff_LowHunger);
+	UKRAINEGTA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(UkraineGTA_Ability_Debuff_LowStamina);
+}
