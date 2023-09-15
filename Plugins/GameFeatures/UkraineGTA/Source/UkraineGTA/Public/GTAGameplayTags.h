e503ced60bba0549deb5061a0150bb0205a0d09c
Anton Zhukov <janton20001@gmail.com>
2023-09-15 18:37:25 +0300

    Update GameplayTags and input configuration

--- Plugins/GameFeatures/UkraineGTA/Source/UkraineGTA/Public/GTAGameplayTags.h
+++ Plugins/GameFeatures/UkraineGTA/Source/UkraineGTA/Public/GTAGameplayTags.h
@@ -8,6 +8,6 @@ namespace GTAGameplayTags
 {
 	UKRAINEGTA_API FGameplayTag FindTagByString(const FString& TagString, bool bMatchPartialString = false);
 
-	UKRAINEGTA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(UkraineGTA_Ability_Debuff_LowHunger);
-	UKRAINEGTA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(UkraineGTA_Ability_Debuff_LowStamina);
+	UKRAINEGTA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Debuff_LowHunger);
+	UKRAINEGTA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Debuff_LowStamina);
 }
