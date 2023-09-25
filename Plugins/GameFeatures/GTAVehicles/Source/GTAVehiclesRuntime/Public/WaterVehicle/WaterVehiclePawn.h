83a2ded1faa32accc51f7a7f29a92af4b6291b68
Anton Zhukov <janton20001@gmail.com>
2023-09-25 19:13:00 +0300

    Implemented basic boat mechanics

---
+++ Plugins/GameFeatures/GTAVehicles/Source/GTAVehiclesRuntime/Public/WaterVehicle/WaterVehiclePawn.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "Interaction/IInteractableTarget.h"
+#include "Interfaces/Vehicle.h"
+#include "WaterVehiclePawn.generated.h"
+
+class UVehicleExtensionComponent;
+class ULyraCameraComponent;
+struct FInputActionValue;
+class ULyraCameraMode;
+
+UCLASS()
+class GTAVEHICLESRUNTIME_API AWaterVehiclePawn : public APawn, public IInteractableTarget, public IVehicle
+{
+	GENERATED_BODY()
+
+public:
+	AWaterVehiclePawn();
+
+	virtual void GatherInteractionOptions(const FInteractionQuery& InteractQuery, FInteractionOptionBuilder& OptionBuilder) override;
+	virtual void CustomizeInteractionEventData(const FGameplayTag& InteractionEventTag, FGameplayEventData& InOutEventData) override;
+
+protected:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void OnVehicleEnter(AActor* CarInstigator, ULyraAbilitySystemComponent* LyraASC);
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void OnVehicleExit(AActor* CarInstigator, ULyraAbilitySystemComponent* LyraASC);
+
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	TSubclassOf<ULyraCameraMode> DetermineCameraMode() const;
+	
+	void Input_Move(const FInputActionValue& InputActionValue);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TObjectPtr<class UStaticMeshComponent> WaterVehicleMesh;
+	
+	UPROPERTY(EditAnywhere, Category="VehicleSettings")
+	FInteractionOption Option;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<ULyraCameraComponent> CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UVehicleExtensionComponent> VehicleExtensionComponent;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Lyra|Camera")
+	TSubclassOf<ULyraCameraMode> CameraMode;
+};
